a4dba72e60fd4ec15dbb6c1d6248a63d5d32dbe7
Kimson M <unbyte@sina.com>
2026-04-09 10:40:52 +0800

    fix array_merge bug.

--- src/Admin.php
+++ src/Admin.php
@@ -542,7 +542,11 @@ class Admin
 
         $jsVariables['pjax_container_selector'] = $pjaxId ? ('#'.$pjaxId) : '';
         $jsVariables['token'] = csrf_token();
-        $jsVariables['lang'] = ($lang = __('admin.client')) ? array_merge($lang, $jsVariables['lang'] ?? []) : [];
+        if (is_array($lang)) {
+            $jsVariables['lang'] = ($lang = __('admin.client')) ? array_merge($lang, $jsVariables['lang'] ?? []) : [];
+        } else {
+            $jsVariables['lang'] = $jsVariables['lang'] ?? [];
+        }
         $jsVariables['colors'] = static::color()->all();
         $jsVariables['dark_mode'] = static::isDarkMode();
         $jsVariables['sidebar_dark'] = config('admin.layout.sidebar_dark') || ($sidebarStyle === 'dark');
